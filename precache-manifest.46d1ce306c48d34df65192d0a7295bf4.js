c882be2ea0595be1c988da3a73360540c35b0bc0
Marina Nosova <nosova_marino4ka@mail.ru>
2019-05-29 21:05:58 +0300

    Updates

---
+++ precache-manifest.46d1ce306c48d34df65192d0a7295bf4.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "967b9323e536bd6e0738ab47d092d58d",
+    "url": "/Klothez-react-redux/index.html"
+  },
+  {
+    "revision": "ea9bbab6dc6f9954d802",
+    "url": "/Klothez-react-redux/static/css/main.4f59122d.chunk.css"
+  },
+  {
+    "revision": "f36eeae9d1dc3968bf07",
+    "url": "/Klothez-react-redux/static/js/2.b8768666.chunk.js"
+  },
+  {
+    "revision": "ea9bbab6dc6f9954d802",
+    "url": "/Klothez-react-redux/static/js/main.3c214282.chunk.js"
+  },
+  {
+    "revision": "8a7ceb1cd7010a5b039f",
+    "url": "/Klothez-react-redux/static/js/runtime~main.452c11a5.js"
+  },
+  {
+    "revision": "b304d0e22959fe3067e6b238b1b853a0",
+    "url": "/Klothez-react-redux/static/media/cart.b304d0e2.svg"
+  },
+  {
+    "revision": "37742cb1adc18d53e7224c7304e995ae",
+    "url": "/Klothez-react-redux/static/media/check.37742cb1.svg"
+  },
+  {
+    "revision": "c99f6f5503680bdd2e4b981dc84a8388",
+    "url": "/Klothez-react-redux/static/media/heart.c99f6f55.svg"
+  },
+  {
+    "revision": "cbc2a86a39e44e837e24ec1f0c2a00f3",
+    "url": "/Klothez-react-redux/static/media/icon-1.cbc2a86a.svg"
+  },
+  {
+    "revision": "b6106674b389076e3e9641de3566e72e",
+    "url": "/Klothez-react-redux/static/media/icon-2.b6106674.svg"
+  },
+  {
+    "revision": "28de95bd8dcabb68d78c62d7e4b54767",
+    "url": "/Klothez-react-redux/static/media/icon-3.28de95bd.svg"
+  },
+  {
+    "revision": "363fe1a11a2c60baadc91c7869898fb4",
+    "url": "/Klothez-react-redux/static/media/icon-4.363fe1a1.svg"
+  },
+  {
+    "revision": "af2c2c02086192909c37321fd2a75322",
+    "url": "/Klothez-react-redux/static/media/img-1.af2c2c02.jpg"
+  },
+  {
+    "revision": "92f11cc34a82fe6778843a4ecb745dea",
+    "url": "/Klothez-react-redux/static/media/img-1@2x.92f11cc3.jpg"
+  },
+  {
+    "revision": "d0797bb1d771b85ea64525adfe071ec8",
+    "url": "/Klothez-react-redux/static/media/img-1@3x.d0797bb1.jpg"
+  },
+  {
+    "revision": "09440481ad5e29ee4a787899903d42ad",
+    "url": "/Klothez-react-redux/static/media/img-2.09440481.jpg"
+  },
+  {
+    "revision": "4adb38d78cf27394f1aa570b1a46403e",
+    "url": "/Klothez-react-redux/static/media/img-2@2x.4adb38d7.jpg"
+  },
+  {
+    "revision": "ad60c22115a53178c59b2f115b77ac5a",
+    "url": "/Klothez-react-redux/static/media/img-2@3x.ad60c221.jpg"
+  },
+  {
+    "revision": "9ee22878d96458a14119ee6fad795686",
+    "url": "/Klothez-react-redux/static/media/logo.9ee22878.svg"
+  },
+  {
+    "revision": "879a4008b4c03c1cb5f66d993097771d",
+    "url": "/Klothez-react-redux/static/media/partner-1@3x.879a4008.png"
+  },
+  {
+    "revision": "7a8226b90480bb084fdbbc557b882218",
+    "url": "/Klothez-react-redux/static/media/partner-2@3x.7a8226b9.png"
+  },
+  {
+    "revision": "ae34248ef3fd81487fa9fb0f153bbddc",
+    "url": "/Klothez-react-redux/static/media/partner-4@3x.ae34248e.png"
+  },
+  {
+    "revision": "10013b6e66b2eed0879e3553de4925ec",
+    "url": "/Klothez-react-redux/static/media/partner-5@3x.10013b6e.png"
+  },
+  {
+    "revision": "ae10bcac75a3ccdeae91ffd39b9c155f",
+    "url": "/Klothez-react-redux/static/media/partner-6@2x.ae10bcac.png"
+  },
+  {
+    "revision": "54f21bfeb84f2d30ea42b78e713ede69",
+    "url": "/Klothez-react-redux/static/media/partner-6@3x.54f21bfe.png"
+  },
+  {
+    "revision": "34e767214f1cd5531b825969b0218f71",
+    "url": "/Klothez-react-redux/static/media/pay-mastercard.34e76721.svg"
+  },
+  {
+    "revision": "b89d7ba913fb87f7421b4d1cb70a68ca",
+    "url": "/Klothez-react-redux/static/media/pay-mir.b89d7ba9.svg"
+  },
+  {
+    "revision": "9b83f7e1de357b5285160ad7270e1ceb",
+    "url": "/Klothez-react-redux/static/media/pay-visa.9b83f7e1.svg"
+  },
+  {
+    "revision": "f661a5d5cafc7941b1a6b267ee01b232",
+    "url": "/Klothez-react-redux/static/media/search.f661a5d5.svg"
+  },
+  {
+    "revision": "3b8530a7ddc44503e859c619b9fd6907",
+    "url": "/Klothez-react-redux/static/media/soc-fb.3b8530a7.svg"
+  },
+  {
+    "revision": "5e8033bc0f185f2593fec055d126ee14",
+    "url": "/Klothez-react-redux/static/media/soc-gl.5e8033bc.svg"
+  },
+  {
+    "revision": "7806cb7aab7b223f8ac983998e075fe6",
+    "url": "/Klothez-react-redux/static/media/soc-inst.7806cb7a.svg"
+  },
+  {
+    "revision": "62297454384e2fbe458d8ed8d32b60b6",
+    "url": "/Klothez-react-redux/static/media/soc-yt.62297454.svg"
+  },
+  {
+    "revision": "d34750c82c1987b0b519ed94ecda827d",
+    "url": "/Klothez-react-redux/static/media/user.d34750c8.svg"
+  }
+]);
